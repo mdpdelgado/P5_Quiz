1cc919da661c3f0fc9c6e622d1c096de3b9b6a46
mdpdelgado <mdp4527@gmail.com>
2018-04-29 09:31:16 +0200

    3-marco de vistas

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var partials = require('express-partials');
+
 var index = require('./routes/index');
 
 
@@ -14,12 +16,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(partials());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
